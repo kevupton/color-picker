771619cd79aec9cf295eaa93615e00e886660ef9
Kevin Upton <kevupton@users.noreply.github.com>
2020-05-19 14:48:11 +1000

    Create component.vue

---
+++ component.vue
@@ -0,0 +1,17 @@
+<template>
+  <sketch-picker :value="value" @input="$emit('intput', $event)" />
+</template>
+
+<script>
+module.exports = {
+  components: {
+    "sketch-picker": node_modules['vue-color'].Sketch
+  },
+  props: {
+    value: [String, Object]
+  },
+};
+</script>
+
+<style>
+</style>
