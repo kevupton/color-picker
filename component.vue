a1c62e9e7f90fc29f6cc945cd739204eda68f849
Kevin Upton <kevupton@users.noreply.github.com>
2020-05-20 18:02:24 +1000

    Update component.vue

--- component.vue
+++ component.vue
@@ -1,7 +1,7 @@
 <template>  
   <div>
     <h3 v-if="meta.label">{{ meta.label }}</h3>
-    <sketch-picker :value="value" v-on:input="$emit('intput', $event)" />
+    <sketch-picker :value="value" @input="$emit('input', $event)" />
   </div>
 </template>
 
